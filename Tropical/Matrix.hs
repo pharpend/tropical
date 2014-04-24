6f67c396012932554175a7a99de14c8ae7ce366b
Peter Harpending <pharpend2@gmail.com>
2014-04-23 19:58:11 -0600

    Added a minimal Matrix module, move the README back to HTML.
    The markdown README just wasn't rendering properly, so I moved it back to
    HTML.
    
    The matrix module is just a type wrapper, declaring that a tropical matrix is
    just a normal matrix with tropical values.
    
    I need to build in unit tests, probably with QuickCheck.
    
    I also removed the equality requirement from tropicality, because it is implied
    by the sortability requirement.

--- Tropical/Matrix.hs
+++ Tropical/Matrix.hs
@@ -1,4 +1,4 @@
--- Tropical.Tropical - the definition of tropical
+-- Tropical.Matrix - a module for matrices.
 --
 -- Copyright (c) 2014, Peter Harpending. <pharpend2@gmail.com>
 -- All rights reserved.
@@ -28,14 +28,12 @@
 -- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 -- OF THE POSSIBILITY OF SUCH DAMAGE.
 
--- |Module for tropical things
-module Tropical.Tropical where
+{-# LANGUAGE ImpredicativeTypes #-}
 
--- |The tropical semiring is {R, infinity, <+>, <*>}. This class says
--- that <+> and <*> need to be defined for any tropical *thing*, be it
--- a scalar, vector, matrix, what have you. It also requires that you
--- be able to determine if two tropical things are equal to each
--- other, and that the two tropical things be sortable.
-class Ord t => Tropical t where
-  (<+>) :: t -> t -> t
-  (<*>) :: t -> t -> t
+-- |Matrices with tropical numbers
+module Tropical.Matrix where
+
+import qualified Data.Matrix as Mat
+import Tropical.Tropical
+
+type Matrix = (Tropical t) => Mat.Matrix t
