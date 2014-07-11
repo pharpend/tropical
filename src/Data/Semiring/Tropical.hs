673461a7de3bf47c5e735f8fe31ff1e4db5056b5
Peter Harpending <pharpend2@gmail.com>
2014-07-11 12:37:22 -0600

    Rewrite the Tropical definition to be a data type.

--- src/Data/Semiring/Tropical.hs
+++ src/Data/Semiring/Tropical.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE InstanceSigs #-}
+
 {-|
 Module       : Data.Semiring.Tropical
 Description  : The definition of tropicality
-Copyright    : (c) Peter Harpending, 2014
+Copyright    : 2014, Peter Harpending.
 License      : BSD3
-Maintainer   : pharpend2@gmail.com
+Maintainer   : Peter Harpending <pharpend2@gmail.com
 Stability    : experimental
 Portability  : Linux
 
@@ -14,11 +16,41 @@ any tropical object has to be an ordered semiring.
 
 module Data.Semiring.Tropical where
 
+import Data.Ord
 import Data.Semiring
 
--- |The tropical semiring is {R union {infinity}, '.+.', '.*.'}. This
--- class says that '.+.' and '.*.' need to be defined for any tropical
--- *thing*, be it a scalar, vector, matrix, what have you. It also
--- requires that you be able to determine if two tropical things are
--- equal to each other, and that the two tropical things be sortable.
-class (Ord t, Semiring t) => Tropical t
+-- |Tropical numbers are like real numbers, except zero is the same
+-- thing as Infinity, and you can't subtract.
+data Real t => Tropical t = Tropical { realValue :: t } -- ^Any tropical number
+                          | Infinity                    -- ^Infinity
+  deriving (Eq, Ord, Show)
+
+type Operator a = a -> a -> a
+
+instance Real a => Semiring (Tropical a) where
+  -- |Tropical addition is the same as taking the minimum
+  a .+. b = min a b
+  -- |Tropical multiplication is the same as the sum
+  a .*. b
+    | Infinity==a || Infinity==b = Infinity
+    | otherwise                  = Tropical $ (realValue a) + (realValue b)
+
+  -- |Infinity acts like zero.
+  zero = Infinity
+  -- |Zero acts like one.
+  one = Tropical 0
+
+-- |Tropical division. Remember, if Infinity is tropical zero, then
+-- you can't divide by it!
+(./.) :: Real a => Operator (Tropical a)
+_ ./. Infinity          = undefined
+Infinity ./. b          = Infinity
+a ./. b                 = Tropical $ (realValue a) - (realValue b)
+
+-- -- |Tropical exponentiation - same as classical multiplication. A
+-- -- mildly interesting correlary is that tropical exponentiation is
+-- -- commutative. That is, y .^. x = x .^. y, for x and y tropical.
+(.^.) :: Real a => Operator (Tropical a)
+a .^. b
+  | Infinity==a || Infinity==b  = Infinity
+  | otherwise                   = Tropical $ (realValue a) * (realValue b)
