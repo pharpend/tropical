c2da6e935eda7688ce72f0b260670eea96730547
Peter Harpending <pharpend2@gmail.com>
2014-07-13 14:45:33 -0600

    Pedantic changes to the skeleton file.

--- skel/Skel.hs
+++ skel/Skel.hs
@@ -1,10 +1,10 @@
 -- |
 -- Module       : Data.Semiring.Tropical.Skel
 -- Description  : A module for Tropical whatever
--- Copyright    : (c) Peter Harpending, 2014
+-- Copyright    : 2014, Peter Harpending
 -- License      : BSD3
--- Maintainer   : pharpend2@gmail.com
+-- Maintainer   : Peter Harpending <pharpend2@gmail.com>
 -- Stability    : experimental
--- Portability  : Linux
+-- Portability  : archlinux
 
 module Data.Semiring.Tropical.Skel where
