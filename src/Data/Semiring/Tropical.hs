9ce7b2f9888107827e55288b4b6d6d9b6515f6bf
Peter Harpending <pharpend2@gmail.com>
2014-07-13 17:46:42 -0600

    Added `logT` function, lots more documentation.

--- src/Data/Semiring/Tropical.hs
+++ src/Data/Semiring/Tropical.hs
@@ -15,7 +15,7 @@ Tropical numbers form a 'Semiring'. Semirings are like
 you can't subtract.
 
 The Tropical semiring, or 𝕋, is {ℝ ∪ {∞}, ⊕, ⊙}. Those are, in Haskell
-terms, 'Real', 'Infinity', '(.+.)', and '(.*.)', respectively.
+terms, @Real@, @Infinity@, @(.+.)@, and @(.*.)@, respectively.
 
 Tropical addition and multiplication are
 
@@ -33,18 +33,23 @@ module Data.Semiring.Tropical
     Tropical(..)
 
   -- ** Tropical operations
+  -- *** Artifacts of 'Semiring'
+  -- $semiring-add
   , (.+.)
+  , zero
+  -- $semiring-mult
   , (.*.)
+  , one
+  -- *** Tropical-exclusive operations
   , (./.)
   , (.^.)
+  , logT
+  -- $ln
 
   -- ** Helper Things
   , Operator
   , TropicalOperator
-  , zero
-  , one
   )
-
 where
 
 import Data.Semiring
@@ -60,13 +65,14 @@ type Operator a = a -> a -> a
 -- |An operator over something tropical
 type TropicalOperator a = Operator (Tropical a) 
 
--- | Some notes - 
--- 
+-- $semiring-add
 -- Tropical addition is the same as taking the minimum. Because
 -- 
 -- min {a, ∞} = a, ∀ a ∈ ℝ
 -- 
 -- 'Infinity' is the additive identity, or 'zero', in Semiring terms. 
+
+-- $semiring-mult
 -- 
 -- Tropical multiplication is the sum. Because 
 -- 
@@ -88,7 +94,9 @@ instance Real a => Semiring (Tropical a) where
   one = Tropical 0
 
 -- |Tropical division. Remember, if Infinity is tropical zero, then
--- you can't divide by it!
+-- you can't divide by it! So,
+-- 
+-- > _ ./. Infinity          = undefined
 (./.) :: Real a => TropicalOperator a
 _ ./. Infinity          = undefined
 Infinity ./. _          = Infinity
@@ -96,8 +104,45 @@ a ./. b                 = Tropical $ (realValue a) - (realValue b)
 
 -- |Tropical exponentiation - same as classical multiplication. A
 -- mildly interesting correlary is that tropical exponentiation is
--- commutative. That is, y .^. x = x .^. y, for x and y tropical.
+-- commutative. That is, @y .^. x = x .^. y@, ∀ x, y ∈ 𝕋
 (.^.) :: Real a => TropicalOperator a
 a .^. b
   | Infinity==a || Infinity==b  = Infinity
   | otherwise                   = Tropical $ (realValue a) * (realValue b)
+
+-- |Tropical logarithm - which is roughly equal to classical
+-- division. That is, @logT a b = b / a@ . Note that, in the context
+-- of ℝ, you can't divide by zero, and there's no well-defined way of
+-- dividing by infinity, so
+--
+-- > logT (Tropical 0) _ = undefined
+-- > logT Infinity _     = undefined
+-- 
+-- However, it is well defined to have infinity or zero in the
+-- divisor, so
+-- 
+-- > logT _ Infinity     = Infinity
+-- > logT _ (Tropical 0) = Tropical 0
+-- 
+-- If you aren't dealing with infinity or zero, this is the normal definition
+-- 
+-- > logT (Tropical base) (Tropical value) = Tropical $ value / base
+-- 
+logT :: (Fractional a, Real a) => TropicalOperator a
+logT (Tropical 0) _ = undefined
+logT Infinity _     = undefined
+logT _ Infinity     = Infinity
+logT _ (Tropical 0) = Tropical 0
+logT (Tropical base) (Tropical value) = Tropical $ value / base
+
+
+-- $ln
+-- 
+-- Normally, when you think of logarithms, you think of e, the
+-- standard log base. e is interesting for many reasons, but chiefly
+-- because D(e^x) = e^x.  Deriviatives rely on differentials, which
+-- are traditionally defined with subtraction.
+-- 
+-- Because there is no subtraction with semirings, the concept of a
+-- deriviative doesn't exist, and therefore the concept of e doesn't
+-- exist. So, there isn't any "natural tropical logarithm."
